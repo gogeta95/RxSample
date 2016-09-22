8a4869010fe41faf36c65036d5dc2bd705d28133
Saurabh Singhal <singhalsaurabh95@gmail.com>
2016-09-23 00:38:41 +0530

    feat: added rxjava implementation of retrofit.

--- app/src/main/java/com/example/saurabh/rxsample/rest/GitHubClient.java
+++ app/src/main/java/com/example/saurabh/rxsample/rest/GitHubClient.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import com.example.saurabh.rxsample.R;
 
 import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 /**
@@ -21,6 +22,7 @@ public class GitHubClient {
     public static WebService getClient(Context context){
         if (webService==null){
             Retrofit retrofit=new Retrofit.Builder()
+                    .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
                     .baseUrl(context.getString(R.string.server_url))
                     .addConverterFactory(GsonConverterFactory.create())
                     .build();
--- app/src/main/java/com/example/saurabh/rxsample/rest/WebService.java
+++ app/src/main/java/com/example/saurabh/rxsample/rest/WebService.java
@@ -2,9 +2,9 @@ package com.example.saurabh.rxsample.rest;
 
 import com.example.saurabh.rxsample.data.SearchResponse;
 
-import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Query;
+import rx.Observable;
 
 /**
  * Created by saurabh on 22/09/16.
@@ -12,5 +12,5 @@ import retrofit2.http.Query;
 
 public interface WebService {
     @GET("/search/repositories")
-    Call<SearchResponse> getSearchResults(@Query("q") String query);
+    Observable<SearchResponse> getSearchResults(@Query("q") String query);
 }
